20859c7677d44c50cb1d73492f2e81c118f339c7
KhanAdnan1 <ka4180910@gmail.com>
2024-09-28 11:18:38 +0530

    commit

--- src/routes/addRestaurant.routes.js
+++ src/routes/addRestaurant.routes.js
@@ -1,10 +1,7 @@
 import express from 'express';
-<<<<<<< HEAD
+
 import { registerRestaurant } from '../controllers/addRestaurant.controllers.js'; // Adjust path if necessary
 
-=======
-import registerRestaurant from '../controllers/addRestaurant.controllers';
->>>>>>> 671346b85a9314cbc4674ac24d917d0fdef1ead5
 const router = express.Router();
 
 router.post('/register', registerRestaurant);
