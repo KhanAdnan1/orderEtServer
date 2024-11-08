fddc1d29000c9d8d40ab6f687525757e423e6217
KhanAdnan1 <ka4180910@gmail.com>
2024-11-08 11:22:06 +0530

    latest commit

--- src/controllers/salesPerson.controllers.js
+++ src/controllers/salesPerson.controllers.js
@@ -7,7 +7,7 @@ import ApiResponse from "../utils/ApiResponse.js";
 const registerSalesPerson = asyncHandler(async (req, res) => {
     const { email, salesPersonNumber, salesPersonName, userName, password } = req.body;
 
-    console.log(email, salesPersonNumber)
+    console.log(email, salesPersonNumber,salesPersonName, userName, password)
     if (
         [email, salesPersonNumber, salesPersonName, userName, password].some((field) =>
             field?.trim() === "")
