9cdf85c07fccedf6c150c9c04fe8e4ed326cf66b
garvrathi <125799111+garvrathi@users.noreply.github.com>
2024-11-18 22:20:59 +0530

    add restuarant functionality.

--- src/controllers/addRestaurant.controllers.js
+++ src/controllers/addRestaurant.controllers.js
@@ -3,40 +3,55 @@ import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 
-
-
 // Controller to register a new restaurant
 const registerRestaurant = asyncHandler(async (req, res) => {
-    const { RestaurantManagerName, RestaurantName, ManagerContact, RestaurantAddress, restaurantAddedBy } = req.body;
+  const {
+    RestaurantManagerName,
+    RestaurantName,
+    ManagerContact,
+    RestaurantAddress,
+  } = req.body;
+  const restaurantAddedBy = req.user?.id;
+  if (!restaurantAddedBy) {
+    throw new ApiError(401, "Unauthorized: User information is missing");
+  }
+  console.log("restaurant added by: ", restaurantAddedBy);
 
-    console.log(RestaurantName, RestaurantManagerName);
+  // Validate if all required fields are provided
+  if (
+    !RestaurantManagerName?.trim() ||
+    !RestaurantName?.trim() ||
+    !ManagerContact?.trim() ||
+    !RestaurantAddress?.trim() ||
+    !restaurantAddedBy
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
 
-    // Validate if all required fields are provided
-    if (
-        [RestaurantManagerName, RestaurantName, ManagerContact, RestaurantAddress, restaurantAddedBy].some((field) => 
-        field?.trim() === "")) {
-        throw new ApiError(400, "All fields are required");
-    }
+  // Create a new restaurant in the database
+  const restaurant = await Restaurant.create({
+    RestaurantManagerName,
+    RestaurantName,
+    ManagerContact,
+    RestaurantAddress,
+    restaurantAddedBy,
+  });
 
-    // Create a new restaurant in the database
-    const restaurant = await Restaurant.create({
-        RestaurantManagerName,
-        RestaurantName,
-        ManagerContact,
-        RestaurantAddress,
-        restaurantAddedBy
-    });
+  // Fetch the newly created restaurant
+  const createdRestaurant = await Restaurant.findById(restaurant._id).populate(
+    "restaurantAddedBy",
+    "salesPersonName"
+  );
 
-    // Fetch the newly created restaurant
-    const createdRestaurant = await Restaurant.findById(restaurant._id).populate('restaurantAddedBy', 'salesPersonName');
+  if (!createdRestaurant) {
+    throw new ApiError(500, "Something went wrong while adding the restaurant");
+  }
 
-    if (!createdRestaurant) {
-        throw new ApiError(500, "Something went wrong while adding the restaurant");
-    }
-
-    // Return a successful response
-    return res.status(200).json(
-        new ApiResponse(200, createdRestaurant, "Restaurant added successfully")
+  // Return a successful response
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(200, createdRestaurant, "Restaurant added successfully")
     );
 });
 
